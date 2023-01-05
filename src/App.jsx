36d1d65c31ce45b131bfa1fadd98589aa09bf069
Sachin Mishra <msachin786.pm@gmail.com>
2023-01-05 08:23:06 +0530

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,20 @@ import React from "react";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
-import Experience from "./components/experience/Experience";
+import Skills from "./components/experience/Skills";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
-// import Fullpage, {
-//   FullPageSections,
-//   FullpageSection,
-//   FullpageNavigation,
-// } from "@ap.cx/react-fullpage";
 
 const App = () => {
   return (
-    <div>
+    <>
       <Nav />
-      <Header/>
+      <Header />
       <About />
-      <Experience/>
-      <Portfolio/>
-      <Contact/>
-      {/* <Fullpage>
-        <FullpageNavigation />
-        <FullPageSections>
-          <FullpageSection>
-            <Header />
-          </FullpageSection>
-          <FullpageSection>
-            <About />
-          </FullpageSection>
-          <FullpageSection>
-            <Experience />
-          </FullpageSection>
-          <FullpageSection>
-            <Portfolio />
-          </FullpageSection>
-          <FullpageSection>
-            <Contact />
-          </FullpageSection>
-        </FullPageSections>
-      </Fullpage> */}
-    </div>
+      <Skills />
+      <Portfolio />
+      <Contact />
+    </>
   );
 };
 
