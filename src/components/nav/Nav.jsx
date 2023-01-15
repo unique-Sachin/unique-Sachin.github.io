1bacf466ef16011c8bb4b4271dd064b1710ba40a
Sachin Mishra <msachin786.pm@gmail.com>
2023-01-15 15:57:03 +0530

    updated

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -14,35 +14,50 @@ const Nav = () => {
     <nav id="nav">
       <Link
         to="header"
-        onClick={() => setActiveNav("#")}
+        onClick={() => {
+          setActiveNav("#");
+          document.title = "Sachin ~ Hero";
+        }}
         className={activeNav === "#" ? "active" : ""}
       >
         <AiOutlineHome />
       </Link>
       <Link
         to="about"
-        onClick={() => setActiveNav("#about")}
+        onClick={() => {
+          setActiveNav("#about");
+          document.title = "Sachin ~ About";
+        }}
         className={activeNav === "#about" ? "active" : ""}
       >
         <AiOutlineUser />
       </Link>
       <Link
         to="experience"
-        onClick={() => setActiveNav("#experience")}
+        onClick={() => {
+          setActiveNav("#experience");
+          document.title = "Sachin ~ Skills";
+        }}
         className={activeNav === "#experience" ? "active" : ""}
       >
         <BiBook />
       </Link>
       <Link
         to="portfolio"
-        onClick={() => setActiveNav("#portfolio")}
+        onClick={() => {
+          setActiveNav("#portfolio");
+          document.title = "Sachin ~ Portfolio";
+        }}
         className={activeNav === "#portfolio" ? "active" : ""}
       >
         <RiServiceLine />
       </Link>
       <Link
         to="contact"
-        onClick={() => setActiveNav("#contact")}
+        onClick={() => {
+          setActiveNav("#contact");
+          document.title = "Sachin ~ Contact";
+        }}
         className={activeNav === "#contact" ? "active" : ""}
       >
         <BiMessageSquareDetail />
