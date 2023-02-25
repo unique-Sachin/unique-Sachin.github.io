a110243fbb9dc09b5f0c6c56324c738e7cb9d13e
Sachin Mishra <msachin786.pm@gmail.com>
2023-02-25 18:02:28 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+
+
 const container = document.getElementById("root");
 
 const root = createRoot(container);
